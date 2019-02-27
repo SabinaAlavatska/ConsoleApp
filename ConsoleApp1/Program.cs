f43be2792018685fafb43261a92389bfb0c91281
SabinaAlavatska <47834291+SabinaAlavatska@users.noreply.github.com>
2019-02-27 11:39:53 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 5 numbers");
+            int[] array = new int[5];
+
+           for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = GetNumber();
+            }
+            
+           for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] *= 10);
+            }
+        }
+        public static int GetNumber()
+        {
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+    }
+}
