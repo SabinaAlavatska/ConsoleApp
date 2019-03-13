48193ffce2d80936888951be97f7afbe394c637c
SabinaAlavatska <47834291+SabinaAlavatska@users.noreply.github.com>
2019-03-13 22:22:53 +0200

    commit message

---
+++ ConsoleApp1/HometaskOneOptional/HometaskOneOptional.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    [TestFixture]
+    public class HometaskOneOptional
+    {
+        ChromeDriver chromeDriver = new ChromeDriver();
+        string url = "http://fortune.com/40-under-40/";
+
+        [SetUp]
+        public void SetUp ()
+        {
+            chromeDriver.Manage().Timeouts().ImplicitWait = new TimeSpan(0,0,0,2);
+            chromeDriver.Manage().Window.Maximize();
+            chromeDriver.Navigate().GoToUrl(url);
+        }
+
+        [Test]
+        public void GoToUrl ()
+        {
+            PageObject pageObject = new PageObject(chromeDriver);
+            string expectedText = pageObject.firstArticleHeader.Text;
+            pageObject.firstArticleLink.Click();
+
+            string actualText = pageObject.clickedArticleHeader.Text;
+            bool isContainsString = actualText.Contains(expectedText);
+            Assert.IsTrue(isContainsString, "Selected article name does not match with opened article page title");
+        }
+
+        [TearDown]
+        public void Clear ()
+        {
+            chromeDriver.Quit();
+        }
+    }
+}
---
+++ ConsoleApp1/HometaskOneOptional/PageObject/StartPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support;
+using SeleniumExtras.PageObjects;
+using OpenQA.Selenium;
+
+namespace ConsoleApp1
+{
+    class PageObject
+    {
+        public PageObject (ChromeDriver chromeDriver)
+        {
+            PageFactory.InitElements(chromeDriver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//*[@data-article-id]")]
+        public IWebElement firstArticleLink { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "fullname")]
+        public IWebElement firstArticleHeader { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "franchise-person-name")]
+        public IWebElement clickedArticleHeader { get; set; }
+    }
+}
