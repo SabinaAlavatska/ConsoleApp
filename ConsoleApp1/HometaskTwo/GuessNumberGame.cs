851ecb34dde2831bba52618e7a8fe7f5f32a88fb
SabinaAlavatska <47834291+SabinaAlavatska@users.noreply.github.com>
2019-03-20 01:24:22 +0200

    fix errors

---
+++ ConsoleApp1/HometaskTwo/GuessNumberGame.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleApp1.HometaskThree;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace ConsoleApp1
+{
+    class GuessNumberGame
+    {
+
+        public static Random rnd = new Random();
+        int guessableNumber = rnd.Next();
+
+        public void GuessNumber()
+        {
+            Console.WriteLine("Please enter a number");
+            int usersNumber;
+
+            do
+            {
+                usersNumber = GetUserNumber();
+                if (guessableNumber > usersNumber)
+                {
+                    Console.WriteLine("Youre number is less than guessable. Try a bigger one!");
+                }
+                else if (guessableNumber < usersNumber)
+                {
+                    Console.WriteLine("Youre number is more than guessable. Try a smaller one!");
+                }
+            }
+            while (guessableNumber != usersNumber);
+            Console.WriteLine("You've guessed the number. You win!");
+        }
+
+        public int GetUserNumber()
+        {
+            int result = 0;
+            while (!int.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Wrong input");
+            }
+            return result;
+        }
+
+        public static void Main(string [] args)
+        {
+            //Program program = new Program();
+           // program.FillArray();
+
+           // GuessNumberGame guessNumberGame = new GuessNumberGame();
+           // guessNumberGame.GuessNumber();
+
+            HometaskThreeGenerics hometaskThree = new HometaskThreeGenerics();
+            hometaskThree.GetInputList();
+            hometaskThree.DisplayInput();
+        }
+    }
+}
+
